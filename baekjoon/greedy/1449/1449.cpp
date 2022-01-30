21ef4dbffc9c3e7e4fb7aba44e9d83970a0dc30d
seonghunn <4ngle86@naver.com>
2022-01-31 06:12:02 +0900

    1/30

---
+++ baekjoon/greedy/1449/1449.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    int N, L;
+    cin >> N >> L;
+    for (int i = 0; i < N; i++)
+    {
+        int num;
+        cin >> num;
+        v.push_back(num);
+    }
+}
