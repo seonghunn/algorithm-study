a028729285fab572f735050b7b6020f004cf087d
seonghunn <4ngle86@naver.com>
2022-02-08 04:21:01 +0900

    2/8

---
+++ baekjoon/DP/1912*/1912.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[100000];
+int dp[100000];
+int N;
+
+//전형적인 dp 유형
+//연속합과 새로운원소 중 큰 것을 골라 dp 배열에 넣으며 maximum을 갱신해 나간다. 새로운원소를 고른다는 것은 그 자리에서 연속합이 끊겨야 최댓값이 나온다는 뜻이다.
+//혼자인게 더 큰가 vs 합해야 더 큰가 중 하나를 선택하는 것
+int main()
+{
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+        dp[i] = arr[i];
+    }
+    int maximum = dp[0];
+    for (int i = 1; i < N; i++)
+    {
+        dp[i] = max(dp[i - 1] + arr[i], arr[i]);
+        if (maximum < dp[i])
+            maximum = dp[i];
+    }
+
+    cout << maximum << "\n";
+
+    return 0;
+}
