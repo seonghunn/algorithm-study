037d6ab6bc4256f10612aa9863bd9a7b40161417
seonghunn <4ngle86@naver.com>
2022-02-25 04:21:44 +0900

    폴더 정리

--- 이코테/ch3.greedy/3-3.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-N,M=map(int,sys.stdin.readline().split())
-card=[list(map(int,sys.stdin.readline().split())) for i in range(N)]
-for i in range (N):
-    card[i].sort()
-    
-minlist=[card[i][0] for i in range(N)]
-print(max(minlist))
